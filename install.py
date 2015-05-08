394470a4385b6943000026df3f282f583c3410a7
Brian Blais <bblais@gmail.com>
2015-05-08 11:17:46 -0400

    added home directory install option

--- install.py
+++ install.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 import os
+import sys
+import argparse
 
-os.system('python setup.py install')
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--home', help='install in home directory',action="store_true")
+args = parser.parse_args()
+
+if args.home:
+    cmd='python setup.py install --home=~'
+else:
+    cmd='python setup.py install'
+
+print cmd
+os.system(cmd)
